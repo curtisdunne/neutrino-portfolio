8484c53fa6bca26884d64e2b3f64e2aaed4b6e5e
Curtis Dunne <curtdunne@gmail.com>
2019-05-22 10:49:26 -0600

    implemented default values for main and thumb images in the Portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,11 +1,18 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
 
+  after_initialize :set_defaults
+
   def self.angular
     where(subtitle: "Angular")
   end
 
   def self.ruby_on_rails
     where(subtitle: "Ruby On Rails")
-  end 
+  end
+
+  def set_defaults
+    self.main_image ||= "https://via.placeholder.com/600x400"
+    self.thumb_image ||= "https://via.placeholder.com/350x200"
+  end
 end
