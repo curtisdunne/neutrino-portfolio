9118374e2c4d5b223cd0e5a078d15050e970b691
Curtis Dunne <curtdunne@gmail.com>
2019-07-09 16:08:43 -0600

    added messages to portfolios and blogs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -59,4 +59,23 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator(alert)
+    end
+  end
+
+  def alert_generator(msg)
+    js add_gritter(msg, title: "Neutrino Software Company", sticky: false)
+  end
+
+
+
+
+
+
+
+
 end
