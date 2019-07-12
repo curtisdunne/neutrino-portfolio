ba9f0979c3d65fc9ec3dfe666c46b6481ec76f50
Curtis Dunne <curtdunne@gmail.com>
2019-07-12 11:46:11 -0600

    implemented jquery functions for action cable comment feature

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,14 +10,15 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require popper
-//= require rails-ujs
 //= require jquery
+//= require bootstrap-sprockets
+//= require jquery_ujs
 //= require jquery-ui
+//= require html.sortable
+//= require popper
 //= require cocoon
 //= require gritter
 //= require cable
-//= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
 //= require_tree .
