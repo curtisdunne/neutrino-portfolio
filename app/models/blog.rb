c35f8331a768bb5a0bf90ee9c4092191daa0bc00
Curtis Dunne <curtdunne@gmail.com>
2019-07-13 13:22:53 -0600

    created topic index and show pages

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -7,7 +7,7 @@ class Blog < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
 
-  validates_presence_of :title, :body
+  validates_presence_of :title, :body, :topic_id
 
   has_many :comments, dependent: :destroy
 
